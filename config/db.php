039d0230cc6fcc18cb8c5f8cd5561318434d59b1
Tyler Morrison <tyler@counterpointstudio.com>
2016-07-20 19:00:55 -0400

    Updated comment in db config

--- config/db.php
+++ config/db.php
@@ -3,7 +3,7 @@
  * Database Configuration
  *
  * All of your system's database configuration settings go in here.
- * You can see a list of the default settings in craft/app/etc/config/defaults/db.php
+ * You can see a list of the default settings in app/etc/config/defaults/db.php
  */
 
  return [
